425cd0f23442fead089224beebd8585b19bfe6fe
xindaowei0 <xindaowei@qq.com>
2024-05-04 04:06:33 +0800

    更新 AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,17 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_dagu.mk
+    $(LOCAL_DIR)/twrp_dagu.mk \
+    $(LOCAL_DIR)/omni_dagu.mk \
+
 
 COMMON_LUNCH_CHOICES := \
-    omni_dagu-user \
-    omni_dagu-userdebug \
-    omni_dagu-eng
+    twrp_dagu-user \
+    twrp_dagu-userdebug \
+    twrp_dagu-eng \
+  

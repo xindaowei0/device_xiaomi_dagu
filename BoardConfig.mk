c17a23a563901df02c380e91b010a476d7f83c44
辛道韦 <151035881+xindaowei0@users.noreply.github.com>
2024-05-06 06:18:04 +0800

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,12 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 DEVICE_PATH := device/xiaomi/dagu
+
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
